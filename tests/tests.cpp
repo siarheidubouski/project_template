b198b894812116f37a2b7f47e4a649bd059b3aa5
imperin <siarhei.dubouski@gmail.com>
2021-01-25 00:40:57 -0800

    gtests were added

---
+++ tests/tests.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+
+unsigned int Fibonacci(unsigned int number) {
+  if (number == 0) return 0;
+  return number == 1 ? 1 : Fibonacci(number - 1) + Fibonacci(number - 2);
+}
+
+TEST (FibonacciTest) {
+  
+  EXPECT_EQ(0, Fibonacci(0));
+  EXPECT_EQ(1, Fibonacci(1));
+  EXPECT_EQ(5, Fibonacci(5));
+  EXPECT_EQ(8, Fibonacci(21));
+}
