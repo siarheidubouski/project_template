e96785ca458accf484cdf6b98ae55231308bdd09
imperin <siarhei.dubouski@gmail.com>
2021-01-24 23:19:14 -0800

    qt was added

---
+++ src/qt/HelloWorldMain.cpp
@@ -0,0 +1,11 @@
+#include "HelloWorld.hpp"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+  QApplication app(argc, argv);
+  HelloWorld dialog;
+  dialog.show();
+
+  return app.exec();// this runs the main event loop and sees to it that cleanup is done
+}
